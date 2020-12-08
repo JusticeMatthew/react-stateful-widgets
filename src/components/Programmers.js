d5dd4e9d2bef4ce4685855799716dcd00c9a2bc2
Matthew Justice <matthewajustice@gmail.com>
2020-12-07 19:32:32 -0500

    Refactored with better understanding

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -36,8 +36,7 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    const arr = devList.filter(dev => dev.id === featuredDev);
-    return arr[0].name
+    return devList[featuredDev-1].name;
   };
 
   const style = {
